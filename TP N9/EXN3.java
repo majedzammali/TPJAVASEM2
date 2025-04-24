e2e670f8060d7ce0459fb653c070a196fe7f4113
majedzammali <majedzammali123@gmail.com>
2025-04-24 13:25:20 +0100

    termin

--- TP N9/EXN3.java
+++ TP N9/EXN3.java
@@ -1,3 +1,10 @@
+import java.util.Scanner;
+
 public class EXN3 {
-    
+    public static void main(String[] args) {
+    int n;
+    n=methode.lectureN();
+    int som=methode.calculesomme(n);
+    System.out.println("la somme est " + som);
+    }
 }
--- TP N9/methode.java
+++ TP N9/methode.java
@@ -11,18 +11,22 @@ public class methode {
     public static int lectureN (){
         Scanner scanner = new Scanner(System.in);
         int n;
-        do{
-            System.out.println("entrez un entier strictement positif  :");
-            while(!scanner.hasNextInt()){
-                System.out.println("Erreur : veuillez entrer un entier valide.");
-                scanner.next();
-            }
-            n= scanner.nextInt();
-            if(n <= 0){
-                System.out.println("Erreur : L'entier doit etre strictement positif .");
-            }
-        }while(n <= 0);
-        return n;
+        try {
+            do{
+                System.out.println("entrez un entier strictement positif  :");
+                while(!scanner.hasNextInt()){
+                    System.out.println("Erreur : veuillez entrer un entier valide.");
+                    scanner.next();
+                }
+                n= scanner.nextInt();
+                if(n <= 0){
+                    System.out.println("Erreur : L'entier doit etre strictement positif .");
+                }
+            }while(n <= 0);
+            return n;
+        } finally {
+            scanner.close();
+        }
     }
     public static int compter (int n){
         int nomberchiffer = 0;
@@ -31,5 +35,25 @@ public class methode {
             nomberchiffer++;
         }
         return nomberchiffer;
+    }
+    
+    public static int calculesomme (int n){
+       int som = 0;
+       for (int i = 0; i < n; i++){
+          som = som + i*i;
+       }
+       return som;
+    }
+    //methode estAmstrong
+    public static boolean estAmstrong(int n){
+        int s = 0;
+        int aux = n;
+        while(n != 0){
+            int r = n % 10;
+            s = s + r*r*r;
+            n = n/10;
+        }
+        return aux == s;
+        
     }
 }
